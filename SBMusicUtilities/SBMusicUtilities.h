0c90d64571caf52793f5500b1b17ed4909b3fcb3
Sam Bender <rednebmas@gmail.com>
2016-03-24 17:40:24 -0700

    Initial Commit

---
+++ SBMusicUtilities/SBMusicUtilities.h
@@ -0,0 +1,19 @@
+//
+//  SBMusicUtilities.h
+//  SBMusicUtilities
+//
+//  Created by Sam Bender on 3/24/16.
+//  Copyright © 2016 Sam Bender. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SBMusicUtilities.
+FOUNDATION_EXPORT double SBMusicUtilitiesVersionNumber;
+
+//! Project version string for SBMusicUtilities.
+FOUNDATION_EXPORT const unsigned char SBMusicUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBMusicUtilities/PublicHeader.h>
+
+
